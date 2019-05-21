ade0c5f61b6a14c997fe7680e2eccaeced19e8c7
Wei Gao <wgao19@gmail.com>
2019-05-21 23:39:28 +0800

    Fix site logo complaint

--- gatsby-config.js
+++ gatsby-config.js
@@ -93,13 +93,8 @@ module.exports = {
         display: "minimal-ui",
         icons: [
           {
-            src: "/logos/logo-192x192.png",
-            sizes: "192x192",
-            type: "image/png"
-          },
-          {
-            src: "/logos/logo-512x512.png",
-            sizes: "512x512",
+            src: "/logos/logo-1024.png",
+            sizes: "1024x1024",
             type: "image/png"
           }
         ]
